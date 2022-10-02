84ff1dae827f84a3db49b3b6c8e213611b9b6e86
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-02 11:49:17 +0545

    Improvement: Knowledge of describe, code covergae are implemented

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,28 @@
 import { render , screen} from "@testing-library/react"
 import { Greet } from "./greet"
 
-test('Greet renders Correctly',()=>{
-    render(<Greet/>)
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument()
-} );
 
-test('Greet renders with a name',()=>{
-    render(<Greet name='Anish'/>);
-    const textElement = screen.getByText('Hello Anish');
-    expect(textElement).toBeInTheDocument()
+//Group testing
+describe('Greet', ()=>{
+    test('Greet renders Correctly',()=>{
+        render(<Greet/>)
+        const textElement = screen.getByText(/Hello/i)
+        expect(textElement).toBeInTheDocument()
+    } );
+    
+    describe('Nested',()=>{
+        it('Greet renders with a name',()=>{
+            render(<Greet name='Anish'/>);
+            const textElement = screen.getByText('Hello Anish'|| 'Hello Guest');
+            expect(textElement).toBeInTheDocument()
+        }) 
+    })
+    
 })
+
+/*test.skip - to skip the test */
+/* test.only - to perform that only that test in the page */
+/*we can also use .skip and  .only method with describe */
+/*Alternative of test method is it*/
+/*Alternative of test.only => fit */
+/*Alternative of test.skip => xit */
