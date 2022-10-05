d1bb0f3169513b42ae5fa7283ee9b4e6e2bb5b87
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-05 09:12:14 +0545

    Improvement: Knowlwdgr of query multiple elements implemented

---
+++ src/components/application/Application.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
+
+describe('Application',()=>{
+    test('renders correctly',()=>{
+        render(<Application/>);
+
+        const pageHeading = screen.getByRole('heading',{
+            // name:'Job application form'
+            level:1
+        });        
+        expect(pageHeading).toBeInTheDocument();
+
+        const sectionHeading = screen.getByRole('heading',{
+            level:2
+        });
+        expect(sectionHeading).toBeInTheDocument();
+        
+        //getBylabelText
+        const nameElement2 = screen.getByLabelText('Name',{
+            selector:'input'
+        });
+        expect(nameElement2).toBeInTheDocument();
+
+        const nameElement3 = screen.getByPlaceholderText('Fullname');
+        expect(nameElement3).toBeInTheDocument();
+
+        const paragraphElement = screen.getByText('All fields are mandatory');
+        expect(paragraphElement).toBeInTheDocument();
+
+        //in case of sigle textbox role element
+        // const nameElement = screen.getByRole('textbox');
+        // expect(nameElement).toBeInTheDocument();
+
+        const nameElement = screen.getByRole('textbox',{
+            name:"Name"
+        });
+        expect(nameElement).toBeInTheDocument();
+
+        const bioElement = screen.getByRole('textbox',{
+            name:'Bio'
+        });
+        expect(bioElement).toBeInTheDocument();
+
+        const jobLocationElement = screen.getByRole('combobox');
+        expect(jobLocationElement).toBeInTheDocument();
+
+        const termsElement = screen.getByRole('checkbox');
+        expect(termsElement).toBeInTheDocument();
+
+        const submitButtonElement = screen.getByRole('button');
+        expect(submitButtonElement).toBeInTheDocument();
+    })
+})
---
+++ src/components/skills/Skills.test.tsx
@@ -0,0 +1,18 @@
+import { screen, render } from "@testing-library/react";
+import { Skills } from "./Skills";
+
+describe("Skills",()=>{
+    const skills = ["HTML","CSS","JS"];
+
+    test("renders correctly", ()=>{
+        render(<Skills skills={skills}/>);
+        const listElement = screen.getByRole("list");
+        expect(listElement).toBeInTheDocument();
+    });
+    
+    test("renders a list of skills", ()=>{
+        render(<Skills skills={skills}/>);
+        const listElements = screen.getAllByRole('listitem');
+        expect(listElements).toHaveLength(skills.length);
+    })
+})
